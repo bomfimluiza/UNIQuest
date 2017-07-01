ee9cdfb790d82e21c67b9ae334fb8d84e0560241
Matheus Silva <sousasilvamatheus@gmail.com>
2017-07-01 11:15:31 -0300

    Subindo repositório

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,5 +10,37 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+}
 
+@Component({
+  template: `
+  <ion-header>
+    <ion-navbar>
+      <button ion-button menuToggle>
+        <ion-icon name="menu"></ion-icon>
+      </button>
+      <ion-title>
+        UNIQuest
+      </ion-title>
+    </ion-navbar>
+  </ion-header>
+
+  <ion-content></ion-content>`
+})
+class TabContentPage{
+  constructor() {}
+}
+
+@Component({
+  template: `
+  <ion-tabs>
+    <ion-tab tabIcon="contact" [root]="rootPage"></ion-tab>
+    <ion-tab tabIcon="compass" [root]="rootPage"></ion-tab>
+    <ion-tab tabIcon="analytics" [root]="rootPage"></ion-tab>
+  </ion-tabs>`
+})
+class TabIconsPage{
+  constructor() {
+    this.rootPage = TabContentPage;
+  }
 }
